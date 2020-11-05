35ab7d0050049bd7a1b873c3735605534a2b3f28
Edree Huss <edreeshuss@gmail.com>
2020-11-05 16:32:04 -0500

    [Thursday] complete CSS and tweak JS code.

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-window.onload = function () {
+      let youDoWantTo = document.querySelector('#yes');
+      youDoWantTo.addEventListener('click', () => {
+            document.getElementById('music').play();
+            document.getElementById('page1').style.display = 'none';
+            document.getElementById('page2').style.display = 'grid';
+      })
 
-    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-          't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
-    var categories;         
-    var chosenCategory;    
-    var getHint ;         
-    var word ;              
-    var guess ;             
-    var geusses = [ ];    
-    var lives ;             
-    var counter ;           
-    var space;             
-  
-   
-    var showLives = document.getElementById("mylives");
-    var showCatagory = document.getElementById("scatagory");
-    var getHint = document.getElementById("hint");
-    var showClue = document.getElementById("clue");
-  
- 
+      let youDoWantTo = document.querySelector('#no')
+      youDoWantTo.addEventListener('click', () => {
+            window.location.href= 'https://www.amc.com/shows/the-walking-dead--119';
+      })
+
+      let turnToPage3 = document.getElementById('introtalk1')
+      turnToPage3.addEventListener('click', () => {
+            document.getElementById('page2').style.display = 'none';
+            document.getElementById('page3').style.display = 'grid';
+      })
+
+      let turnToPage4 = document.getElementById('introtalk2')
+      turnToPage4.addEventListener('click', () => {
+            document.getElementById('page3').style.display = 'none';
+            document.getElementById('page4').style.display = 'grid';
+      })
+
+      let turnToPage5 = document.getElementById('introtalk3')
+      turnToPage5.addEventListener('click', () => {
+            document.getElementById('page4').style.display = 'none';
+            document.getElementById('page5').style.display = 'grid';
+      })
+
+      let wordsetarr = ['rick', 'carl', 'walkers', 'shane', 'cdc', 'biters', 'daryl', 'governor', 'merle', 'michonne', 'glenn', 'prison', 'alexandria', 'alpha', 'negan', 'kingdom', 'woodbury', 'sanctuary' ];
+
+      let threeLettersBack ='DEFGHIJKLMNOPQRSTUVWXYZABC';
+
+      let decodeAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+      let randomWord = wordsetarr[Math.floor(Math.random() * wordsetarr.length)]
+      console.log('${randomWord}');
+
+      
+      
+
+
+}
